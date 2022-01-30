bcb68c60d734ce2bac3d7b8605f50b5d239186ea
Brian <briankemboirotich@gmail.com>
2022-01-30 09:58:31 +0000

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,53 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+""" Module for Base """
+
 import uuid
 from datetime import datetime
+from uuid import uuid4
+import models
+import json
+
+format_dt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-class BaseModel():
-    """ User class"""
-
+class BaseModel:
+    """ Basemodel class """
     def __init__(self, *args, **kwargs):
-        if len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            
-
+        """ Initialization of Database """
+        if args is not None and len(args) > 0:
+            pass
+        if kwargs:
+            for key, item in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    item = datetime.strptime(item, format_dt)
+                if key not in ['__class__']:
+                    setattr(self, key, item)
         else:
-            self.id = kwargs['id']
-            self.created_at = datetime.strptime(
-                kwargs
-                ['created_at'], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            self.updated_at = datetime.strptime(
-                kwargs
-                ['updated_at'], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-
-    def __str__(self):
-        """ return the tring represantaion of an instance """
-        return "[{}] ({}) {}".format(
-            __class__.__name__,
-            self.id, self.__dict__
-        )
-
-    def save(self):
-        """ update the update time with the current time"""
-
-        self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def to_dict(self):
-        """ Return a  dectionary containing all keys"""
+        """ to_dict definition """
+        
+        dic = {}
+        for key, item in self.__dict__.items():
+            if key in ['created_at', 'updated_at']:
+                dic[key] = item
 
-        created_at = self.created_at.isoformat()
-        updated_at = self.updated_at.isoformat()
-
-        dic = self.__dict__
-        dic['__class__'] = __class__.__name__
-        dic['created_at'] = created_at
-        dic['updated_at'] = updated_at
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
         return dic
+
+
+    def __str__(self):
+        """ str definition """
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
+
+    def save(self):
+        """ Save definition """
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
